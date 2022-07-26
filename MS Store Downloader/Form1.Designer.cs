0d9bcea7b57b8aaaf2d09da048139bc7efdb5544
Pavel Jarkovský <jarkovsky.pavel@gmail.com>
2022-07-26 09:01:14 +0200

    Add project files.

---
+++ MS Store Downloader/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MS_Store_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Application = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(119, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(588, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(713, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Application});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
+            this.listView1.Size = new System.Drawing.Size(695, 397);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Application
+            // 
+            this.Application.Text = "Application";
+            this.Application.Width = 667;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Package URL or ID";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MS_Store_Downloader.Properties.Resources.animated_loading;
+            this.pictureBox1.Location = new System.Drawing.Point(265, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(713, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 55);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "Save Here";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(713, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Beta";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MS Store Downloader";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Application;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MS Store Downloader/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+using System.Threading;
+using Microsoft.Win32;
+using System.Security;
+using HtmlAgilityPack;
+
+namespace MS_Store_Downloader
+{
+    public partial class Form1 : Form
+    {
+        CancellationToken cancel = new CancellationToken();
+        public Form1()
+        {
+            InitializeComponent(); 
+            if (!WBEmulator.IsBrowserEmulationSet(this))
+            {
+                WBEmulator.SetBrowserEmulationVersion(this);
+            }
+            pictureBox1.Location = new Point(this.Width/2 - 64, this.Height/2 - 64);
+            button2.Enabled = false;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => pictureBox1.Visible = true));
+            else
+                pictureBox1.Visible = true;
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => listView1.Items.Clear()));
+            else
+                listView1.Items.Clear();
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button1.Enabled = false));
+            else
+                button1.Enabled = false;
+            string appID = textBox1.Text;
+            if(appID.Contains("/"))
+                appID = appID.Remove(0, appID.LastIndexOf("/") + 1);
+            if (appID.Contains("?"))
+                appID = appID.Remove(appID.IndexOf("?"));
+            HttpClient appIdHttp = new HttpClient(new HttpClientHandler() { AllowAutoRedirect = true, UseCookies = true });
+            FormUrlEncodedContent ucont = new FormUrlEncodedContent(new Dictionary<string, string> { { "type", "ProductId" }, { "url", appID }, { "ring", checkBox1.Checked ? "WIF" : "Retail" }, { "lang", System.Globalization.CultureInfo.InstalledUICulture.Name } });
+                        
+            appIdHttp.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.62");
+            var response = await appIdHttp.PostAsync("https://store.rg-adguard.net/api/GetFiles", ucont , cancel).ConfigureAwait(false);
+            string responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            responseString = responseString.Replace("<head/>", "");
+            HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(responseString);
+
+            foreach (HtmlNode node in htmlDoc.DocumentNode.Descendants("a"))
+            {
+                string text = node.InnerText;
+                string link = node.GetAttributeValue("href", "");
+                if (text.ToLower().EndsWith(".appx") || text.ToLower().EndsWith(".appxbundle") || text.ToLower().EndsWith(".msixbundle"))
+                {
+                    ListViewItem lvi = new ListViewItem(text);
+                    lvi.Tag = link;
+                    if (InvokeRequired)
+                        this.BeginInvoke(new Action(() => listView1.Items.Add(lvi)));
+                    else
+                        listView1.Items.Add(lvi);
+                }
+            }
+            if(InvokeRequired)
+                this.BeginInvoke(new Action(() => pictureBox1.Visible = false));
+            else
+                pictureBox1.Visible = false;
+
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button2.Enabled = true));
+            else
+                button2.Enabled = true;
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button1.Enabled = true));
+            else
+                button1.Enabled = true;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(this.Width/2 - 64, this.Height/2 - 64);
+            listView1.Columns[0].Width = listView1.Width - 27;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => pictureBox1.Visible = true));
+            else
+                pictureBox1.Visible = true;
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button1.Enabled = false));
+            else
+                button1.Enabled = false;
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button2.Enabled = false));
+            else
+                button2.Enabled = false;
+            Dictionary<string, string> toDownload = new Dictionary<string, string>();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = Path.GetDirectoryName(saveFileDialog1.FileName);
+                if (listView1.SelectedItems == null || listView1.SelectedItems.Count == 0)
+                {
+                    foreach (ListViewItem lvi in listView1.Items)
+                    {
+                        toDownload.Add(lvi.Text, (string)lvi.Tag);
+                    }
+                }
+                else
+                {
+                    foreach(ListViewItem lvi in listView1.SelectedItems)
+                    {
+                        toDownload.Add(lvi.Text, (string)lvi.Tag);
+                    }
+                }
+
+                foreach (string s in toDownload.Keys)
+                {
+                    using (HttpClient download = new HttpClient(new HttpClientHandler() { AllowAutoRedirect = true }))
+                    {
+                        using (var result = await download.GetAsync(toDownload[s]).ConfigureAwait(false))
+                        {
+                            using (FileStream fs = new FileStream(path + "/" + s, FileMode.Create))
+                            {
+                                await result.Content.CopyToAsync(fs).ConfigureAwait(false);
+                            }
+                        }
+                    }
+                }
+            }
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => pictureBox1.Visible = false));
+            else
+                pictureBox1.Visible = false;
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button1.Enabled = true));
+            else
+                button1.Enabled = true;
+            if (InvokeRequired)
+                this.BeginInvoke(new Action(() => button2.Enabled = true));
+            else
+                button2.Enabled = true;
+        }
+        
+    }
+
+    public enum BrowserEmulationVersion
+    {
+        Default = 0,
+        Version7 = 7000,
+        Version8 = 8000,
+        Version8Standards = 8888,
+        Version9 = 9000,
+        Version9Standards = 9999,
+        Version10 = 10000,
+        Version10Standards = 10001,
+        Version11 = 11000,
+        Version11Edge = 11001
+    }
+
+    public static class WBEmulator
+    {
+        private const string InternetExplorerRootKey = @"Software\Microsoft\Internet Explorer";
+
+        public static int GetInternetExplorerMajorVersion(Form1 form)
+        {
+            int result;
+
+            result = 0;
+
+            try
+            {
+                RegistryKey key;
+
+                key = Registry.LocalMachine.OpenSubKey(InternetExplorerRootKey);
+
+                if (key != null)
+                {
+                    object value;
+
+                    value = key.GetValue("svcVersion", null) ?? key.GetValue("Version", null);
+
+                    if (value != null)
+                    {
+                        string version;
+                        int separator;
+
+                        version = value.ToString();
+                        separator = version.IndexOf('.');
+                        if (separator != -1)
+                        {
+                            int.TryParse(version.Substring(0, separator), out result);
+                        }
+                    }
+                }
+            }
+            catch (SecurityException)
+            {
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+
+            return result;
+        }
+        private const string BrowserEmulationKey = InternetExplorerRootKey + @"\Main\FeatureControl\FEATURE_BROWSER_EMULATION";
+
+        public static BrowserEmulationVersion GetBrowserEmulationVersion(Form1 form)
+        {
+            BrowserEmulationVersion result;
+
+            result = BrowserEmulationVersion.Default;
+
+            try
+            {
+                RegistryKey key;
+
+                key = Registry.CurrentUser.OpenSubKey(BrowserEmulationKey, true);
+                if (key != null)
+                {
+                    string programName;
+                    object value;
+
+                    programName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+                    value = key.GetValue(programName, null);
+
+                    if (value != null)
+                    {
+                        result = (BrowserEmulationVersion)Convert.ToInt32(value);
+                    }
+                }
+            }
+            catch (SecurityException)
+            {
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+
+            return result;
+        }
+        public static bool SetBrowserEmulationVersion(BrowserEmulationVersion browserEmulationVersion, Form1 form)
+        {
+            bool result;
+
+            result = false;
+
+            try
+            {
+                RegistryKey key;
+
+                key = Registry.CurrentUser.OpenSubKey(BrowserEmulationKey, true);
+
+                if (key != null)
+                {
+                    string programName;
+
+                    programName = Path.GetFileName(Environment.GetCommandLineArgs()[0]);
+
+                    if (browserEmulationVersion != BrowserEmulationVersion.Default)
+                    {
+                        // if it's a valid value, update or create the value
+                        key.SetValue(programName, (int)browserEmulationVersion, RegistryValueKind.DWord);
+                    }
+                    else
+                    {
+                        // otherwise, remove the existing value
+                        key.DeleteValue(programName, false);
+                    }
+
+                    result = true;
+                }
+            }
+            catch (SecurityException)
+            {
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+
+            return result;
+        }
+
+        public static bool SetBrowserEmulationVersion(Form1 form)
+        {
+            int ieVersion;
+            BrowserEmulationVersion emulationCode;
+
+            ieVersion = GetInternetExplorerMajorVersion(form);
+
+            if (ieVersion >= 11)
+            {
+                emulationCode = BrowserEmulationVersion.Version11Edge;
+            }
+            else
+            {
+                switch (ieVersion)
+                {
+                    case 10:
+                        emulationCode = BrowserEmulationVersion.Version10;
+                        break;
+                    case 9:
+                        emulationCode = BrowserEmulationVersion.Version9;
+                        break;
+                    case 8:
+                        emulationCode = BrowserEmulationVersion.Version8;
+                        break;
+                    default:
+                        emulationCode = BrowserEmulationVersion.Version7;
+                        break;
+                }
+            }
+
+            return SetBrowserEmulationVersion(emulationCode, form);
+        }
+        public static bool IsBrowserEmulationSet(Form1 form)
+        {
+            return GetBrowserEmulationVersion(form) != BrowserEmulationVersion.Default;
+        }
+    }
+}
